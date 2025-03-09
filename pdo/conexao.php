48967fd8095846a336b11a1e8e73c89406bd45ea
Luizmagao <luizmagao@gmail.com>
2025-03-09 20:27:03 -0300

    :construction: Criado banco de dados Estudantes

--- pdo/conexao.php
+++ pdo/conexao.php
@@ -2,3 +2,13 @@
 
 $caminho = __DIR__ . '/bancodedados/banco.sqlite';
 $pdo = new PDO('sqlite:' . $caminho);
+
+// criar banco de dados de estudantes
+$pdo->exec("
+    CREATE TABLE estudantes 
+        (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT,
+            data_nascimento TEXT
+        );
+");
