32014bde69258d8d34e1b3cc1b109148d6a5c087
Luizmagao <luizmagao@gmail.com>
2025-03-09 21:24:27 -0300

    :construction: Criando proteção sobre o SQLinject

--- pdo/conexao.php
+++ pdo/conexao.php
@@ -23,12 +23,14 @@ try {
         INSERT INTO estudantes 
         (id, nome, data_nascimento)
         VALUES
-        (NULL, 'Luiz Lins', '1985-10-27'),
-        (NULL, 'Maria', ''); DROP TABLE estudantes; -- Ires', '1954-07-12'),
-        (NULL, 'Iluise Miranda', '1987-08-17'),
-        (NULL, 'João Abílio', '1983-10-02');
+        (?, ?, ?);
     ";
-    echo $pdo->exec($sqlInserir) . PHP_EOL;
+    $statement = $pdo->prepare($sqlInserir);
+    $statement->bindValue(2, 'Sílvio');
+    $statement->bindValue(3, '2025-01-01');
+    if ($statement->execute()) {
+        echo "Estudante inserido." . PHP_EOL;
+    }
 } catch (PDOException $e) {
     echo "Erro ao tentar inserir dados: {$e->getMessage()}" . PHP_EOL;
 }
