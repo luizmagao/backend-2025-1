0670af406cafd9541536413e5abb70fbf499cf1b
Luizmagao <luizmagao@gmail.com>
2025-03-11 23:32:27 -0300

    :sparkles: Criando conexao dentro do repositorio

--- pdo/src/Infra/Estudante/RepositorioEstudantePdo.php
+++ pdo/src/Infra/Estudante/RepositorioEstudantePdo.php
@@ -4,9 +4,17 @@ namespace Luizmagao\PdoSqlite\Infra\Estudante;
 
 use Luizmagao\PdoSqlite\Domain\Model\Estudante;
 use Luizmagao\PdoSqlite\Domain\Model\RepositorioEstudante;
+use PDO;
 
 class RepositorioEstudantePdo implements RepositorioEstudante
 {
+    private PDO $conexao;
+
+    function __construct(PDO $conexao)
+    {
+        $this->conexao = $conexao;
+    }
+
     public function adicionar(Estudante $estudante): Estudante
     {
         return new Estudante('', '', '');
